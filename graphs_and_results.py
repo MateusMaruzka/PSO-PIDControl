37b9d4ed7212f25b53ea1248624a843d082c7c1e
MateusMaruzka <mateusroncaglio@alunos.utfpr.edu.br>
2020-04-30 01:31:19 -0300

    Corrigindo intervalos dos graficos e mudando apresentação do eixo y

--- graphs_and_results.py
+++ graphs_and_results.py
@@ -10,7 +10,10 @@ import glob
 import pickle
 
 import numpy as np
+
 import matplotlib.pyplot as plt 
+import matplotlib.ticker as ticker
+
 import scipy.signal as signal
 
 import pypid_control_lib as pypid
@@ -20,14 +23,13 @@ plt.rcParams["font.family"] = "Times New Roman"
 
 def main():
     
-    
-    
     P = signal.TransferFunction([1],[1, 4, 6, 4, 1])
     files = glob.glob("dados/*.pickle")
     
     
     fig = plt.figure()
     ax = [fig.add_subplot(221+i) for i in range(len(files))]
+    
     for idx, file_name in enumerate(files):
         
         with open(file_name, "rb") as f:
@@ -38,8 +40,9 @@ def main():
             
             print(data.get('Gbest'))
             gb= data.get('Gbest')
-            y,e,u,r,t = pypid.picontrol(P, params.get('Ts'), params.get('Tf'), np.array([gb]), 1, atraso = 0)
+            y,e,u,r,t = pypid.picontrol(P, params.get('Ts'), params.get('Tf')//2, np.array([gb]), 1, atraso = 0)
             print(np.sum(e[0]**2))
+            
             """
             # a,b,c,tp = pypid.step_info(t, y[0])
             # ax[0].annotate(r'$K_p=' + str(round(gb[0],2)) + '$' + '\n'
@@ -51,7 +54,12 @@ def main():
             #                            connectionstyle="angle3"))
             """
             
-            ax[idx].step(t,y[0], '--', label = "ISE "+ r'($\lambda$' + ')', linewidth = 1.2)
+            ll = params.get('Lambda')
+            label = data.get("Metodo").split("_")[0] + "-TVC" + r' ($\lambda = {:.2f}$)'.format(ll) \
+                    if ll != 0 else data.get("Metodo").split("_")[0]
+            
+            print(label)
+            ax[idx].step(t,y[0], '--', label = label, linewidth = 1.2)
             ax[idx].step(t,r[0], 'k--', label = "Referência", linewidth = 1)
             ax[idx].set_ylabel("y(t)")
             ax[idx].set_xlabel("t")
@@ -61,7 +69,19 @@ def main():
             # ax[1].set_ylabel("u(t)")
             # ax[1].set_xlabel("t")
             
-            ax[idx].set_xlim([0, params.get("Tf")])
+            ax[idx].set_xlim([0, params.get("Tf")//2])
+            ax[idx].set_ylim(bottom = 0, top = max(y[0]) + 0.1)
+            
+            
+            # StrMethod formatter
+            # setup(ax)
+            ax[idx].yaxis.set_major_locator(ticker.MultipleLocator(0.25))
+            # ax[idx].yaxis.set_minor_locator(ticker.MultipleLocator(0.05))
+            ax[idx].yaxis.set_major_formatter(ticker.StrMethodFormatter("{x}"))
+         
+            
+            
+            
             plt.tight_layout()            
 
                 
